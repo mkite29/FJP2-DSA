8acb4e170db13303fa857c6e077601e095ce249c
Mohit Kaimwaal <72551003+mkite29@users.noreply.github.com>
2022-01-01 15:19:52 +0530

    Create Print Zigzag.cpp

---
+++ Level 1/Recursion Backtracking/Print Zigzag.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+void pzz(int n){
+    // write your code here
+    if(n == 0)return;
+
+    cout<<n<<" ";
+    pzz(n-1);
+    
+    cout<<n<<" ";
+
+    pzz(n-1);
+    cout<<n<<" ";
+    //cout<<1<<" ";
+    
+}
+
+
+
+int main(){
+    int n; cin>>n;
+    pzz(n);
+}
