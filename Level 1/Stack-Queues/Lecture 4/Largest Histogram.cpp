9eff8c6e08d3f95ac74b8880fb31f3f976e9b2b4
Mohit Kaimwaal <72551003+mkite29@users.noreply.github.com>
2022-02-04 00:20:18 +0530

    Update Largest Histogram.cpp

--- Level 1/Stack-Queues/Lecture 4/Largest Histogram.cpp
+++ Level 1/Stack-Queues/Lecture 4/Largest Histogram.cpp
@@ -1,1 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void display(vector<int>a) {
+  for (int i = 0; i < a.size(); i++)
+    cout << a[i] << "   ";
+  
+}
+
+
+vector<int> nsel(vector<int>arr)
+{
+ vector<int> nge(arr.size(),-1);
+  stack<int> st;
+  
+  for(int i = 0;i<arr.size();i++){
+    
+    while(!st.empty() and arr[i] <= arr[st.top()]){
+      st.pop();
+    }      
+
+    if(st.size() > 0)
+      nge[i] = st.top();
+
+    st.push(i);
+
+    
+  }
+  
+  return nge;
+  
+}
+
+vector<int> nser(vector<int>arr)
+{
+  vector<int> nge(arr.size(),-1);
+  stack<int> st;
+  
+  for(int i = arr.size()-1;i>=0;i--){
+      
+      while(!st.empty() and arr[i] <= arr[st.top()]){
+          st.pop();
+      }
+      
+      if(st.size() > 0){
+          nge[i] =  st.top();
+      }
+      
+      st.push(i);
+    
+  }
+  
+  return nge;
+  
+}
+
+int main(int argc, char** argv)
+{
+  int n;
+  cin >> n;
+
+  vector<int>arr(n, 0);
+  for (int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+
+  vector<int>arr1(n, 0);
+  arr1 = nsel(arr);
+  
+
+ 
+  vector<int>arr2(n, 0);
+  arr2 = nser(arr);
+
+
+
+  int ans = INT_MIN;
+
+  for(int i = 0;i<n;i++){
+
+    if(arr1[i] == -1 and arr2[i] == -1){
+      ans = max(ans,arr[i] * n);
+    }
+    else{
+      ans = max(ans,arr[i] * (arr2[i] - arr1[i] - 1));
+    }
+
+  }
+
+  cout<<ans<<endl;
+  
+  return 0;
+}
