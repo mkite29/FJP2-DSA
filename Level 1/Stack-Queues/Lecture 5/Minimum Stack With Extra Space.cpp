9f4eb3039e5c8b595ee967602b35194f0ae7b5de
Mohit Kaimwaal <72551003+mkite29@users.noreply.github.com>
2022-02-09 22:54:08 +0530

    Update Minimum Stack With Extra Space.cpp

--- Level 1/Stack-Queues/Lecture 5/Minimum Stack With Extra Space.cpp
+++ Level 1/Stack-Queues/Lecture 5/Minimum Stack With Extra Space.cpp
@@ -1,1 +1,40 @@
+class MinStack {
+public:
+    
+    stack<int> minStack;
+    stack<int> mainStack;
+    
+    void push(int val) {
+        
+        if(mainStack.size() == 0){
+            minStack.push(val);
+            mainStack.push(val);
+        }
+        else{
+           minStack.push(min(val,minStack.top()));
+           mainStack.push(val); 
+        }
+    }
+    
+    void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+    
+    int top() {
+        return mainStack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
 
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
