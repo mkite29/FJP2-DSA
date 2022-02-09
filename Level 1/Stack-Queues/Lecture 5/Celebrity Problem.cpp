69e3c4bd35a0af00d9639168abda3c76d7caa94e
Mohit Kaimwaal <72551003+mkite29@users.noreply.github.com>
2022-02-09 22:18:31 +0530

    Update Celebrity Problem.cpp

--- Level 1/Stack-Queues/Lecture 5/Celebrity Problem.cpp
+++ Level 1/Stack-Queues/Lecture 5/Celebrity Problem.cpp
@@ -1,1 +1,53 @@
+https://practice.geeksforgeeks.org/problems/the-celebrity-problem/1/
+    int pop(stack<int> &s){
+	int x = s.top();
+	s.pop();
 
+	return x;
+}
+
+int celeb(vector<vector<int>> &grid){
+
+
+	int r = grid.size();
+
+	stack<int> st;
+	for(int i = 0;i<r;i++)
+		st.push(i);
+	
+	
+	while(st.size() > 1){
+		
+
+		//pop the first two elements
+		int y = pop(st);
+		int x = pop(st);
+		
+		//if x cant be a celeb
+		if(grid[x][y] == 1)
+			st.push(y);
+	
+		else{
+			st.push(x);
+		}
+	}
+
+
+	int x = pop(st);
+	
+	//it's row should be zero
+	for(int i = 0;i<grid.size();i++){
+		if(i == x)continue;
+		if(grid[x][i] == 1)return -1;
+	}
+
+	// //it's row should be zero
+	for(int i = 0;i<grid.size();i++){
+		if(i == x)continue;
+		if(grid[i][x] == 0)return -1;
+	}
+
+	//cout<<x<<endl;
+	return x;
+}
+  
